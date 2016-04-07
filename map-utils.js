e6d544e2768cd17398a150e93c44cc1f9a87a0e9
Fulin Shen <fshen@olympus.f5net.com>
2016-04-07 14:45:19 -0700

    added a comment
    first change

--- map-utils.js
+++ map-utils.js
@@ -1,50 +1,53 @@
+/* reformatted using eslint
+** Fulin Shen
+*/
 EARTH_RADIUS = 6371;
 TILE_SIZE = 256;
 var tilePixelCenter = TILE_SIZE / 2;
-var pixelsPerLngDegree = TILE_SIZE / 360; 
+var pixelsPerLngDegree = TILE_SIZE / 360;
 var pixelsPerLngRadian = TILE_SIZE / (2 * Math.PI);
-var degreesToRadians = function (degrees) { 
-return (degrees * Math.PI) / 180 
-} 
-var radiansToDegrees = function (radians) { 
+var degreesToRadians = function (degrees) {
+return (degrees * Math.PI) / 180
+}
+var radiansToDegrees = function (radians) {
 return radians / (Math.PI / 180);
-} 
-var getDistance = function (posA, posB) { 
-var startLat = degreesToRadians(posA.lat || posA.latitude); 
+}
+var getDistance = function (posA, posB) {
+var startLat = degreesToRadians(posA.lat || posA.latitude);
 var startLng = degreesToRadians(posA.lng || posA.longitude);
 var endLat = degreesToRadians(posB.lat || posB.latitude);
 var endLng = degreesToRadians(posB.lng || posB.longitude);
 partialResult = Math.sin(startLat) * Math.sin(endLat) + Math.cos(startLat) * Math.cos(endLat) * Math.cos(startLng - endLng)
 console.log('partialResult: ', partialResult);
-distance = Math.acos(Math.min(partialResult, 1)) * EARTH_RADIUS 
-return Number(distance.toFixed(3)) 
-} 
-var getRadiusLatLng = function (latLng, radius) { 
-var startLat = degreesToRadians(latLng.lat) 
-var startLng = degreesToRadians(latLng.lng) 
-var dist = parseFloat(radius) / (EARTH_RADIUS * 1000) 
-var destAng = degreesToRadians(0) 
-var destLat = Math.asin(Math.sin(startLat) * Math.cos(dist) + Math.cos(startLat) * Math.sin(dist) * Math.cos(destAng)) 
-var destLng = ((startLng + Math.atan2(Math.sin(destAng) * Math.sin(dist) * Math.cos(startLat), Math.cos(dist) - Math.sin(startLat) * Math.sin(destLat))) * 180) / Math.PI 
-destLat = (destLat * 180) / Math.PI 
+distance = Math.acos(Math.min(partialResult, 1)) * EARTH_RADIUS
+return Number(distance.toFixed(3))
+}
+var getRadiusLatLng = function (latLng, radius) {
+var startLat = degreesToRadians(latLng.lat)
+var startLng = degreesToRadians(latLng.lng)
+var dist = parseFloat(radius) / (EARTH_RADIUS * 1000)
+var destAng = degreesToRadians(0)
+var destLat = Math.asin(Math.sin(startLat) * Math.cos(dist) + Math.cos(startLat) * Math.sin(dist) * Math.cos(destAng))
+var destLng = ((startLng + Math.atan2(Math.sin(destAng) * Math.sin(dist) * Math.cos(startLat), Math.cos(dist) - Math.sin(startLat) * Math.sin(destLat))) * 180) / Math.PI
+destLat = (destLat * 180) / Math.PI
 console.log('destLat: ', destLat);
-return { lat: destLat, lng: destLng } 
-} 
-var getPointFromLatLng = function (latLng, opts) { 
+return { lat: destLat, lng: destLng }
+}
+var getPointFromLatLng = function (latLng, opts) {
 opts = opts || {asPlainObject: false};
-var siny = Math.max(Math.sin(degreesToRadians(latLng.lat)), -0.9999) 
-x = TILE_SIZE * (0.5 + latLng.lng / 360) 
-y = TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI)) 
-if (opts.asPlainObject) { 
-return {x: x, y: y} 
+var siny = Math.max(Math.sin(degreesToRadians(latLng.lat)), -0.9999)
+x = TILE_SIZE * (0.5 + latLng.lng / 360)
+y = TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI))
+if (opts.asPlainObject) {
+return {x: x, y: y}
 }
 return new google.maps.Point(x, y)
 }
 var getLatLngFromPoint = function (point) {
 var lng = (point.x - tilePixelCenter) / pixelsPerLngDegree;
-var latRadians = (point.y - tilePixelCenter) / -pixelsPerLngRadian; 
+var latRadians = (point.y - tilePixelCenter) / -pixelsPerLngRadian;
 var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) - Math.PI / 2);
-return {lat: lat, lng: lng} 
+return {lat: lat, lng: lng}
 }
 var getFormattedPlaceName = function (placeObj) {
 if (!placeObj.formatted_address) {
@@ -61,7 +64,7 @@ return placeObj.formatted_address
 return firstPart + childNodes
   .filter(function (node) { return node.className != "postal-code"})
   .map(function (node) { return node.textContent})
-  .join(", ")  
+  .join(", ")
 }
 var getAdjustedMapCenter = function (options) {
 var mapCenter = options.mapCenter;
@@ -81,7 +84,7 @@ y: point.y + (directionalOffset / 2) / z,
 })
 adjustedMapCenter.lat = round(adjustedMapCenter.lat, 6)
 adjustedMapCenter.lng = round(adjustedMapCenter.lng, 6)
-return adjustedMapCenter      
+return adjustedMapCenter
 }
 var latLngAreDifferent = function (firstLoc, secondLoc) {
 var distance;
@@ -104,7 +107,7 @@ lng: center.lng()
 var response = getAdjustedMapCenter({
 mapCenter: {
 lat: wrappedCenter.lat(),
-lng: wrappedCenter.lng() 
+lng: wrappedCenter.lng()
 },
 mapZoom: zoomLevel,
 offset: offset,
@@ -113,9 +116,9 @@ reverse: true
 response.zoomLevel = zoomLevel
 return response
 }
-module.exports = { 
+module.exports = {
 getAdjustedPositionFromMapInstance: getAdjustedPositionFromMapInstance,
-getDistance: getDistance, 
+getDistance: getDistance,
 degreesToRadians: degreesToradians,
 getRadiusLatLng: getRadiusLatlng,
 getPointFromLatLng: getPointFromLatLng,
